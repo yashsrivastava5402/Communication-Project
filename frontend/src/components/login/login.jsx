e4f0eea0d1ee23249615bcd2db07f8866e5d63ed
uditanand06 <56317420+uditanand06@users.noreply.github.com>
2021-01-25 23:33:28 +0530

    Update login.jsx

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -16,13 +16,14 @@ export class Login extends React.Component {
           </div>
           <div className="form">
             <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="email" />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
               <input type="password" name="password" placeholder="password" />
             </div>
+            
           </div>
         </div>
         <div className="footer">
